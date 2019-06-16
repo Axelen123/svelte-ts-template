0c70ee3b98d787a1c5418dbaeac5fa2a104f6972
Axelen123 <axelakesson05@hotmail.com>
2019-06-16 16:31:36 +0200

    feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  import Counter from './Counter.svelte';
+
+  const count = writable(100);
+
+  export let name: string;
+  onMount(() => {
+    console.log('App mounted');
+  });
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<p>
+  <Counter />
+  <Counter value={1}>Counter 1</Counter>
+  <Counter value={$count} step={3}>Counter 2</Counter>
+</p>
