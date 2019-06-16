cfb052048277261ee0ef9a7464a18d7e9db99146
Axelen123 <axelakesson05@hotmail.com>
2019-06-16 16:35:34 +0200

    feat: use eslint-plugin-svelte3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,23 @@
 module.exports = {
-  parser: 'vue-eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
-    extraFileExtensions: ['.svelte'],
     ecmaVersion: 2019,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   env: {
     es6: true,
-    browser: true,
+    browser: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [ 'plugin:prettier/recommended', 'prettier/@typescript-eslint', 'plugin:@typescript-eslint/recommended' ],
+  plugins: [ '@typescript-eslint', 'svelte3', 'prettier' ],
   rules: {
     'no-shadow': 'warn',
-    'linebreak-style': ['warn', 'unix'],
+    'linebreak-style': [ 'warn', 'unix' ],
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/indent': [ 'warn', 2 ],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'prettier/prettier': 'warn',
+    'prettier/prettier': 'error'
   },
   settings: {
     'svelte3/lint-template': true,
