a0dd3e544dabe95a9e9c23fee2feef89202157e3
Axelen123 <axelakesson05@hotmail.com>
2019-06-19 11:13:41 +0200

    feat: update svelte to 3.5.2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import App from "./App.svelte";
+/// <reference types="svelte" />
+import App from './App.svelte';
 
 const app = new App({
   target: document.body,
   props: {
-    name: "world"
-  }
+    name: 'world',
+  },
 });
 
 export default app;
