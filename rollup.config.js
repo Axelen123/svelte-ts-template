55f81f5e0c0266bbdac61553a7f470c6d0c81cdb
Axelen123 <axelakesson05@hotmail.com>
2019-06-20 20:40:49 +0200

    feat: use preprocessor from svelte.config

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
-import { preprocess, createEnv, readConfigFile } from 'svelte-ts-preprocess';
-
 const production = !process.env.ROLLUP_WATCH;
-
-const env = createEnv();
-const compilerOptions = readConfigFile(env);
-const opts = {
-  env,
-  compilerOptions: {
-    ...compilerOptions,
-    allowNonTsExtensions: true,
-  },
-};
 
 export default {
   input: 'src/main.ts',
@@ -35,7 +23,7 @@ export default {
       css: css => {
         css.write('public/bundle.css');
       },
-      preprocess: preprocess(opts),
+      preprocess: require('./svelte.config').preprocess,
     }),
 
     // If you have external dependencies installed from
